4493f49bed6efffcd721616d13effeeb19f6b2eb
Asiimoviet <asiimoviet@gmail.com>
2023-11-07 17:20:51 -0800

    第一次提交

---
+++ src/main/java/usc/edu/csci201/leetscope/LeetscopeApplication.java
@@ -0,0 +1,16 @@
+package usc.edu.csci201.leetscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Qixuan Chen
+ */
+@SpringBootApplication
+public class LeetscopeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetscopeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/usc/edu/csci201/leetscope/LeetscopeApplicationTests.java
@@ -0,0 +1,15 @@
+package usc.edu.csci201.leetscope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+@SpringBootTest
+class LeetscopeApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assert.isTrue(true, "");
+    }
+
+}
