73d10fe90be05ded6d9154441fd940615c8c00e9
Asiimoviet <asiimoviet@gmail.com>
2023-11-15 17:23:11 -0800

    更新了Login和Register的Controller

--- src/main/java/nenesekai/leetscope/controller/UserController.java
+++ src/main/java/nenesekai/leetscope/controller/UserController.java
@@ -2,10 +2,7 @@ package nenesekai.leetscope.controller;
 
 import nenesekai.leetscope.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import nenesekai.leetscope.entity.User;
 import nenesekai.leetscope.model.Result;
 
@@ -15,13 +12,39 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @PostMapping("/register")
-    public Result<Void> register(@RequestBody() User user) {
+    @PostMapping("/registerLegacy")
+    public Result<Void> registerLegacy(@RequestBody() User user) {
         return userService.register(user);
     }
 
+    @PostMapping("/register")
+    public Result<Void> register(
+            @RequestParam("name") String name,
+            @RequestParam("password") String password,
+            @RequestParam(value = "isTeacher", required = false) Boolean isTeacher,
+            @RequestParam(value = "isStudent", required = false) Boolean isStudent
+    ) {
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+        user.setIsTeacher(isTeacher != null && isTeacher);
+        user.setIsStudent(isStudent != null && isStudent);
+        return userService.register(user);
+    }
+
+    @PostMapping("/loginLegacy")
+    public Result<String> loginLegacy(@RequestBody() User user) {
+        return userService.login(user);
+    }
+
     @PostMapping("/login")
-    public Result<String> login(@RequestBody() User user) {
+    public Result<String> login(
+            @RequestParam("name") String name,
+            @RequestParam("password") String password
+    ) {
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
         return userService.login(user);
     }
 }
--- src/main/java/nenesekai/leetscope/model/Result.java
+++ src/main/java/nenesekai/leetscope/model/Result.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * @author Qixuan Chen
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,18 +16,20 @@ public class Result<T> {
     private T data;
 
     public static <T> Result<T> success() {
-        return new Result<>("SUCCESS", "Success", null);
+        return new Result<>(SUCCESS_CODE, "Success", null);
     }
 
     public static <T> Result<T> success(String msg) {
-        return new Result<>("SUCCESS", msg, null);
+        return new Result<>(SUCCESS_CODE, msg, null);
     }
 
     public static <T> Result<T> success(T data) {
-        return new Result<>("SUCCESS", "Success", data);
+        return new Result<>(SUCCESS_CODE, "Success", data);
     }
 
     public static <T> Result<T> success(String msg, T data) {
-        return new Result<>("SUCCESS", msg, data);
+        return new Result<>(SUCCESS_CODE, msg, data);
     }
+
+    public static final String SUCCESS_CODE = "SUCCESS";
 }
