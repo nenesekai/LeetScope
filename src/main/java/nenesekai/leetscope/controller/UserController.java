ca2397e4adbf97cd748cdd8aeeffe37efbf14517
Asiimoviet <asiimoviet@gmail.com>
2023-11-18 05:04:52 -0800

    新增了whoAmI API用于查询当前登录用户

--- src/main/java/nenesekai/leetscope/controller/UserController.java
+++ src/main/java/nenesekai/leetscope/controller/UserController.java
@@ -1,7 +1,10 @@
 package nenesekai.leetscope.controller;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.annotation.Resource;
+import nenesekai.leetscope.model.NoDataResult;
 import nenesekai.leetscope.service.UserService;
+import nenesekai.leetscope.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import nenesekai.leetscope.entity.User;
@@ -21,5 +24,19 @@ public class UserController {
     @PostMapping("/login")
     public Result login(@RequestBody() User user) {
         return userService.login(user);
+    }
+
+    @GetMapping("/whoAmI")
+    public Result whoAmI(@RequestHeader(name = "Authorization", required = false) String authorization) {
+        if (authorization == null || !authorization.startsWith("Bearer ")) {
+            return NoDataResult.failed(Result.INVALID_PARAM_CODE, "No Token!");
+        }
+        try {
+            Long uid = Long.valueOf(JwtUtil.parseToken(authorization.replace("Bearer ", "")));
+            System.out.println(uid);
+            return userService.getUserById(uid);
+        } catch (ExpiredJwtException e) {
+            return NoDataResult.failed(Result.INVALID_PARAM_CODE, "Token Expired!");
+        }
     }
 }
